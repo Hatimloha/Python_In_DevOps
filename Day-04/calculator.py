91e85f58300e7fd8686f40d1d110d35d2fb35828
Hatimloha <139457449+Hatimloha@users.noreply.github.com>
2024-09-02 21:34:27 +0530

    Create calculator.py

---
+++ Day-04/calculator.py
@@ -0,0 +1,15 @@
+def addition(num1, num2):
+  add = num1 + num2
+  return add
+
+def sub(num1, num2):
+  add = num1 - num2
+  return sub
+
+def addition(num1, num2):
+  mul = num1 * num2
+  return mul
+
+print(addition(5,10))
+print(sub(50,10))
+print(mul(10,20))
