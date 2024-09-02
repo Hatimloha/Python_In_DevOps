fc22786e1ec29d3bf5c842694460d4d1946bcd4f
Hatimloha <139457449+Hatimloha@users.noreply.github.com>
2024-09-02 23:01:52 +0530

    Create api_token.py

---
+++ Day-05/api_token.py
@@ -0,0 +1,15 @@
+# First save the sensitive_information in machine
+export api_token="sd876%^&*&Y567duhdS987trfhn"
+
+# Programm:
+import os
+
+print(os.getenv("api_token"))
+
+
+# Execute file 
+python3 <file name> 
+
+
+# Output
+sd876%^&*&Y567duhdS987trfhn
