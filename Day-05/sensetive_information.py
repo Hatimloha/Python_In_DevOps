bf051aa94587bcf310312057bfc453630f1eadd1
Hatimloha <139457449+Hatimloha@users.noreply.github.com>
2024-09-02 23:00:06 +0530

    Create sensetive_information.py

---
+++ Day-05/sensetive_information.py
@@ -0,0 +1,20 @@
+# First save the sensitive_information in machine
+export password="hatim"
+
+# Programm:
+import os
+
+print(os.getenv("password"))
+
+
+# Run command
+python3 <file name> 
+
+
+
+
+
+
+
+
+
