5b46ebc084918578c6cbd8f350eec628b719f087
Hatimloha <139457449+Hatimloha@users.noreply.github.com>
2024-09-06 16:08:18 +0530

    Create my_set.py

---
+++ Day-11/my_set.py
@@ -0,0 +1,13 @@
+# Define the sets
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+# Perform set operations
+union_set = set1.union(set2)  # Union of sets
+intersection_set = set1.intersection(set2)  # Intersection of sets
+difference_set = set1.difference(set2)  # Difference of sets
+
+# Print results
+print("Union of sets:", union_set)
+print("Intersection of sets:", intersection_set)
+print("Difference of sets:", difference_set)
