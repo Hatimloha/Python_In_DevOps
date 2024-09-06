371918872ac3e48532e9964c39a0516a140f897b
Hatimloha <139457449+Hatimloha@users.noreply.github.com>
2024-09-06 08:30:46 +0530

    Create Dictionary_list.py

---
+++ Day-11/Dictionary_list.py
@@ -0,0 +1,16 @@
+ec2_instances = [
+    {
+        "name": "ec2_1",
+        "Ram": "8 gb"
+    },
+    {
+        "name": "ec2_2",
+        "Ram": "50 gb"
+    },
+    {
+        "name": "ec2_3",
+        "Ram": "5 gb"
+    }
+]
+
+print(ec2_instances[2]["name"])
